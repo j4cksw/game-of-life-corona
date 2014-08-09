7185877ec7ddebdf27e729c4e922411d630b9e79
j4cksw <ninjaadvance@gmail.com>
2014-08-09 15:32:31 +0700

    add test to count live neighbors

--- scripts/BoardIterator.lua
+++ scripts/BoardIterator.lua
@@ -1,11 +1,18 @@
 local BoardIterator = {}
 
+local LiveNeighborCounter = require("scripts.LiveNeighborCounter")
+
 function BoardIterator.next(cellTable)
     for rowIndex, row in pairs(cellTable) do
         for cellIndex, cell in pairs(row) do
-            cell.next()
+            local liveNeighborsCount = LiveNeighborCounter.countNeighborOfCell(cellTable, cellIndex, rowIndex)
+            cell:next(liveNeighborsCount)
         end
-    end 
+    end
+end
+
+function BoardIterator.setLiveNeighborCounter(liveNeighborCounter)
+    LiveNeighborCounter = liveNeighborCounter
 end
 
 return BoardIterator
--- spec/board_iterator_spec.lua
+++ spec/board_iterator_spec.lua
@@ -3,14 +3,39 @@ describe("BoardIterator", function()
     local LiveCell = require("scripts.LiveCell")
     local BoardIterator = require("scripts.BoardIterator")
 
+    local fakeLiveNeighborsCount = 0
+
+    local MockLiveNeighborCounter = {
+        countNeighborOfCell = function()
+            return fakeLiveNeighborsCount
+        end
+    }
+
+    before_each(function()
+        spy.on(MockLiveNeighborCounter, "countNeighborOfCell")
+        BoardIterator.setLiveNeighborCounter(MockLiveNeighborCounter)
+    end)
+
+    it("should count the live neighbors of each cell", function()
+        local cellTable = {
+            { LiveCell:new() }
+        }
+
+        BoardIterator.next(cellTable)
+
+        assert.spy(MockLiveNeighborCounter.countNeighborOfCell).was_called_with(cellTable, 1, 1)
+    end)
+
     it("call next on each cell and assign to new board", function()
         local cellTable = {
             { LiveCell:new() }
         }
         stub(cellTable[1][1], "next")
 
+        fakeLiveNeighborsCount = 255
+
         BoardIterator.next(cellTable)
 
-        assert.spy(cellTable[1][1].next).was_called()
+        assert.spy(cellTable[1][1].next).was_called_with(cellTable[1][1], fakeLiveNeighborsCount)
     end)
 end)
