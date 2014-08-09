ab3dab5b9e8b0745324cd082de72bd6c8ed4c462
j4cksw <ninjaadvance@gmail.com>
2014-08-09 08:39:12 +0700

    extrac rendering each cell from BoardRenderer to CellRenderer

--- scripts/BoardRenderer.lua
+++ scripts/BoardRenderer.lua
@@ -1,11 +1,15 @@
 local BoardRenderer = {}
 
+local CellRenderer
 function BoardRenderer.renderFromTable(initCells)
     local initX = 32
     for index, cell in pairs(initCells) do
-        local cellRect = display.newRect(32+64*(index-1), 32, 64, 64)
-        cellRect:setFillColor(1,1,1)
+        CellRenderer.render(cell, 32+64*(index-1), 32, 64, 64)
     end
+end
+
+function BoardRenderer.setCellRenderer(cellRenderer)
+    CellRenderer = cellRenderer
 end
 
 return BoardRenderer
--- spec/board_renderer_spec.lua
+++ spec/board_renderer_spec.lua
@@ -1,47 +1,25 @@
 describe("BoardRenderer", function()
 
-    local fakeRect = {
-        setFillColor = function()end
-    }
-
-    display = {
-        newRect = function()
-            return fakeRect
-        end
-    }
-
     local LiveCell = require("scripts.LiveCell")
 
-    local boardRenderer = require("scripts.BoardRenderer")
+    local BoardRenderer = require("scripts.BoardRenderer")
+
+    local FakeCellRenderer = {
+        render = function()end
+    }
 
     before_each(function()
-        spy.on(display, "newRect")
-        spy.on(fakeRect, "setFillColor")
+        spy.on(FakeCellRenderer, "render")
+        BoardRenderer.setCellRenderer(FakeCellRenderer)
     end)
 
-    it("should not draw any cell when given empty table", function()
-        local initCells = {}
-
-        boardRenderer.renderFromTable(initCells)
-
-        assert.spy(display.newRect).was_not_called()
-    end)
-
-    it("should draw cell ractangle when given table", function()
-        local initCells = { LiveCell:new() }
-
-        boardRenderer.renderFromTable(initCells)
-
-        assert.spy(display.newRect).was_called_with(32, 32, 64, 64)
-    end)
-
-    it("should draw cell ractangle when given table", function()
+    it("should call CellRenderer with calculated points", function()
         local initCells = { LiveCell:new(), LiveCell:new() }
 
-        boardRenderer.renderFromTable(initCells)
+        BoardRenderer.renderFromTable(initCells)
 
-        assert.spy(display.newRect).was_called_with(32, 32, 64, 64)
-        assert.spy(display.newRect).was_called_with(96, 32, 64, 64)
+        assert.spy(FakeCellRenderer.render).was_called_with(initCells[1], 32, 32, 64, 64)
+        assert.spy(FakeCellRenderer.render).was_called_with(initCells[2], 96, 32, 64, 64)
     end)
 
 end)
