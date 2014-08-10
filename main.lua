055bb10c17ae9b11382b7117225971247c708762
j4cksw <ninjaadvance@gmail.com>
2014-08-10 09:56:33 +0700

    test glider

--- main.lua
+++ main.lua
@@ -5,11 +5,21 @@ local LiveCell = require("scripts.LiveCell")
 local DeadCell = require("scripts.DeadCell")
 local BoardIterator = require("scripts.BoardIterator")
 
-local cellTable = {
-    { LiveCell:new(), LiveCell:new(), LiveCell:new(), LiveCell:new() },
-    { LiveCell:new(), DeadCell:new(), DeadCell:new(), LiveCell:new() },
-    { LiveCell:new(), LiveCell:new(), LiveCell:new(), LiveCell:new() },
-}
+local cellTable = {}
+
+for i = 1, (display.viewableContentHeight/64) do
+    table.insert(cellTable, {})
+
+    for j = 1, (display.viewableContentWidth/64) do
+        table.insert(cellTable[i], DeadCell:new())
+    end
+end
+
+cellTable[1][3] = LiveCell:new()
+cellTable[2][1] = LiveCell:new()
+cellTable[2][3] = LiveCell:new()
+cellTable[3][2] = LiveCell:new()
+cellTable[3][3] = LiveCell:new()
 
 BoardRenderer.renderFromTable(cellTable)
 
